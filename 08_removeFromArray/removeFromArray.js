dd4389fee88c7d0a8f30a60c2ea70b1939bfb86c
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-12-24 11:00:46 +0600

    08 Task's Test1 DONE

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removedValue) {
+    result = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] !== removedValue) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
