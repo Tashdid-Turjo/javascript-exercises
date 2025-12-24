e00690937a29f738a7d27f96303460e5055d7fc1
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-12-24 13:07:19 +0600

    08 Task's Test1 DONE

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let result = 0;
 
+    for(let i = firstNum; i <= secondNum; i++) {
+        result += i;
+    }
+    return result;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
