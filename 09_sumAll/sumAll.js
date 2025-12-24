11414b130aafd270e94ff528cc1978ea6e1597bc
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-12-24 14:05:48 +0600

    08 Task's Test4 DONE

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,22 +1,24 @@
 const sumAll = function(firstNum, secondNum) {
     let result = 0;
 
-    if(firstNum < secondNum) {
+    if((firstNum < secondNum) && (firstNum > 0) && (secondNum > 0)) {
         for(let i = firstNum; i <= secondNum; i++) {
             result += i;
         }   
     return result;
     }
 
-    if(secondNum < firstNum) {
+    if((secondNum < firstNum) && (firstNum > 0) && (secondNum > 0)) {
         for(let i = secondNum; i <= firstNum; i++) {
             result += i;
         }   
     return result;
     }
+
+    else return 'ERROR';
 };
 
-console.log(sumAll(123, 1));
+console.log(sumAll(-10, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
