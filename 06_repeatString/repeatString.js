5bbbaf9819daadc122a50be4bdb337fa32f3709f
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-12-24 09:25:41 +0600

    06 Task DONE

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,8 +1,10 @@
-str = "";
+// let result = "";     // If result is global, it keeps old data. Next function call adds more, so output becomes too long. Thus, it will show in the output (times * 2) times.
 
 const repeatString = function(str, times) {
+    let result = "";
+
+    if (times < 0) return "ERROR";
     for (let i = 0; i < times; i++) {
-        let result;
         result += str;
     }
     return result;
