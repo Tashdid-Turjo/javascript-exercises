03e0636493b84860f85cada5fecafa88e98641b8
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-12-24 09:55:56 +0600

    07 Task DONE

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+// !! N.B: in the spec/test file, removing .skip, will show in the output that, every Tests ran, otherwise it will show in the output that, Tests: 3 skipped, 1 passed. !!
 
+const reverseString = function(str) {
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        result += str[str.length - 1 - i];
+    }
+
+    return result;
 };
+
+console.log(reverseString('123! abc! Hello, Odinite.'));
 
 // Do not edit below this line
 module.exports = reverseString;
