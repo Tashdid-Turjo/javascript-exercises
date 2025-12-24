87455b41448fad39b7aa7f10cd417cd6069e6ac5
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-12-24 11:15:10 +0600

    08 Task's Test3 DONE

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,17 +1,17 @@
 // !! 3 arguments are there now; where for first Test, there was two arguments.
 
-const removeFromArray = function(arr, removedValue1, removedValue2) {
+const removeFromArray = function(arr, removedValue1) {
     result = [];
 
     for(let i = 0; i < arr.length; i++) {
-        if ((arr[i] !== removedValue1) && (arr[i] !== removedValue2)) {
+        if (arr[i] !== removedValue1) {
             result.push(arr[i]);
         }
     }
     return result;
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+console.log(removeFromArray([1, 2, 2, 3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
